6462dbda88221ca5c98fb870cc3ae1e3ac57002c
iminsgineark <ankurjha4025@gmail.com>
2025-01-24 13:17:30 +0000

    removed un-used code (for B/G image)

--- app.py
+++ app.py
@@ -43,15 +43,3 @@ if st.button('Predict Probability'):
     st.header(f"{batting_team}: {r_2}%")
     st.header(f"{bowling_team}: {r_1}%")
 
-# Adding background
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #f0f2f6;
-    }
-    
-    </style>
-    """,
-    unsafe_allow_html=True
-)
